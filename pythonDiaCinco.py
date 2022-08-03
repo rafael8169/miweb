a03b92c6ba6b353f0d90cf1420b19047635cc094
Rtamduq <rtamduq@gmail.com>
2022-08-03 11:22:24 -0500

    Realice cambios en los archivos pythonDiaCinco insertando un nuevo elemento En el otro archivo pythonDiaCinco2 realice cambios crando un shuffle a la lista de nombres

--- pythonDiaCinco.py
+++ pythonDiaCinco.py
@@ -1,3 +1,5 @@
 lista = [1,2,3,4]
 
 print(type(lista))
+lista.append(5)
+print(lista)
--- pythonDiaCinco2.py
+++ pythonDiaCinco2.py
@@ -1,7 +1,11 @@
-from random import randint
+from random import randint, shuffle
 
 lista = [1,2,3,4]
 
 lista2 = randint(lista[0],lista[3])
-
 print(f"este es el numero aleatorio {lista2}")
+
+palabras = ['rafael','diana','juan']
+shuffle(palabras)
+print(f"estas apalabras fueron shuffled {palabras}")
+
